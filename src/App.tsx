0a98502dc6c31310c0c6d3be8eb6801d9f33e118
exit <saraswathi.mac@gmail.com>
2023-08-03 20:38:58 +0530

    Added main and app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Library Management</h1>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/NavBar";
+import { Loader } from "./components/Loader/Loader";
+
+import AddBook from "./pages/AddBook";
+import AddUser from "./pages/User/AddUser";
+
+const App = () => {
+  return (
+    <>
+      <Loader showLoading />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<AddBook />} />
+          <Route path="/user" element={<AddUser />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        backgroundColor: "white",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
+);
