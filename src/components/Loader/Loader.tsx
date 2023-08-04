442e2805b6cf3b52154c9f54db29441d806687ed
exit <saraswathi.mac@gmail.com>
2023-08-04 18:31:45 +0530

    Updated the loader function

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,46 +1,29 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import { Box, CircularProgress } from "@chakra-ui/react";
 
-type Props = {
-  showLoading?: boolean;
-};
-
-const Loader: FC<Props> = ({ showLoading = false }: Props) => {
-  const [showLoader, setShowLoader] = useState(showLoading);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowLoader(false);
-    }, 1000 * 2);
-  }, [showLoader]);
+export const Loader: FC = () => {
   return (
-    <>
-      {showLoader && (
-        <Box
-          backgroundColor={"black"}
-          opacity={"0.85"}
-          position={"fixed"}
-          top={"0"}
-          left={"0"}
-          bottom={"0"}
-          width={"100%"}
-          zIndex="50"
-          overflow={"hidden"}
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          flexDirection={"column"}
-        >
-          <CircularProgress
-            isIndeterminate
-            color="red"
-            size="100px"
-            thickness="6px"
-          />
-        </Box>
-      )}
-    </>
+    <Box
+      backgroundColor={"black"}
+      opacity={"0.85"}
+      position={"fixed"}
+      top={"0"}
+      left={"0"}
+      bottom={"0"}
+      width={"100%"}
+      zIndex="50"
+      overflow={"hidden"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+    >
+      <CircularProgress
+        isIndeterminate
+        color="red"
+        size="100px"
+        thickness="6px"
+      />
+    </Box>
   );
 };
-
-export { Loader };
